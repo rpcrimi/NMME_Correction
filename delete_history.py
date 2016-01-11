c8a91d5abe4bf1b69f990b99a763798e1c92611a
Robert Crimi <rpcrimi@gmail.com>
2016-01-11 10:18:05 -0700

    fixed bug

---
+++ delete_history.py
@@ -0,0 +1,24 @@
+import re
+import os
+
+# Return a list of all netCDF files in "direrctory"
+def get_nc_files(directory, dstFolder, regexFilter):
+	print "Gathering Files..."
+
+	matches = []
+	# Do a walk through input directory
+	for root, dirnames, files in os.walk(directory):
+		# Find all filenames with .nc type
+		for filename in files:
+			filename = os.path.join(root, filename)
+			#if filename.endswith(('.nc', '.nc4')) and re.match(regexFilter, filename):
+			if filename.endswith(('.nc')) and re.match(regexFilter, filename):
+				dstFileName = dstFolder + filename
+				if not os.path.isfile(dstFileName):
+					# Add full path of netCDF file to matches list
+					matches.append(filename)
+	return matches
+
+r = re.compile(".*198[0-9].*")
+files = get_nc_files("../../convert_nc3_pad/NASA-GMAO/", "NONE", r)
+print files
