ba0866a88b1e9a02e385ac37f127bfd3193295b4
Robert Crimi <rocr5180@colorado.edu>
2016-01-18 16:05:02 -0700

    integer error

--- convert_nc4.py
+++ convert_nc4.py
@@ -4,50 +4,43 @@ import sys
 import shlex
 import argparse
 import subprocess
+from progressbar import *
 
-def convert_file(inputFile):
-	print "Original File:\t%s" % (outputFile)
+sys.stdout.flush()
 
-	outputFile = inputFile.split(".nc")[0] + ".nc4"
-	'''
-	call = "nccopy -k 4 -d 2 %s %s" % (inputFile, outputFile)
-	p = subprocess.Popen(shlex.split(call.encode('ascii')))
-	returnCode = p.returncode
+def convert_file(inputFile, dstDir):
+	newFile = dstDir + inputFile.split("incoming/")[-1] + "4"
+	directory = ""
 
-	out = "None"
-	while out:
-		call = "/usr/sbin/lsof"
-		grep = "grep %s" % (inputFile)
-		p2 = subprocess.Popen(call, stdout=subprocess.PIPE)
-		p3 = subprocess.Popen(shlex.split(grep), stdin=p2.stdout, stdout=subprocess.PIPE)
-		p2.stdout.close()
-		out, err = p3.communicate()
-		p3.stdout.close()
-		returnCode = p.returncode
-	'''
-	print "New File:\t%s" % (outputFile)
+	for folder in newFile.split("/")[:-1]:
+		directory += folder + "/"
+		if not os.path.exists(directory):
+			print "Creating folder:\t%s" % (directory)
+			os.makedirs(directory)
+
+	print "Converting File:\t%s" % (newFile)
+	call = "nccopy -k 4 -d 2 %s %s" % (inputFile, newFile)
+	p = subprocess.check_call(shlex.split(call.encode('ascii')))
 
 
-# Return a list of all netCDF files in "direrctory"
+# Return a list of all netCDF files in "directory"
 def get_nc_files(directory, regexFilter):
 	print "Gathering Files..."
-	if ".nc" in directory:
-		return [directory]
-	else:
-		matches = []
-		# Do a walk through input directory
-		for root, dirnames, files in os.walk(directory):
-			# Find all filenames with .nc type
-			for filename in files:
-				filename = os.path.join(root, filename)
-				if filename.endswith(('.nc', '.nc4')) and re.match(regexFilter, filename):
-					matches.append(filename)
-		return matches
+	matches = []
+	# Do a walk through input directory
+	for root, dirnames, files in os.walk(directory):
+		# Find all filenames with .nc type
+		for filename in files:
+			filename = os.path.join(root, filename)
+			if filename.endswith('.nc') and re.match(regexFilter, filename):
+				matches.append(filename)
+	return matches
 
 
 def main():
 	parser = argparse.ArgumentParser(description='Pad Header Script')
-	parser.add_argument("-s", "--scrDir",   dest="scrDir",   help = "Directory to pad headers")
+	parser.add_argument("-s", "--scrDir",   dest="scrDir",   help = "Directory to convert files")
+	parser.add_argument("-d", "--dstDir",   dest="dstDir",   help = "Diretory to move files to")
 	parser.add_argument("-f", "--filter",   dest="filter",   help = "Regex filter for filenames")
 	
 	args = parser.parse_args()
@@ -61,8 +54,15 @@ def main():
 			regexFilter = re.compile(".*")
 
 		files = get_nc_files(args.scrDir, regexFilter)
+
+		i = 1
+		widgets = ['Percent Done: ', Percentage(), ' ', AnimatedMarker(), ' ', ETA()]
+		bar = ProgressBar(widgets=widgets, maxval=len(files)).start()
 		for f in files:
-			convert_file(f)
+			convert_file(f, args.dstDir)
+			bar.update(i)
+			i = i + 1
+		bar.finish()
 
 if __name__ == "__main__":
 	main()
