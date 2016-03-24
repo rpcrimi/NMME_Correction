bb45e775f93cfe68f655ee006e8a31e88dad8a7c
Robert Crimi <rpcrimi@gmail.com>
2016-03-24 11:11:11 -0600

    added fixes to table

--- renamefolders.py
+++ renamefolders.py
@@ -19,14 +19,14 @@ def main():
 		for filename in files:
 			filename = os.path.join(root, filename)
 			if filename.endswith(('.nc')) and re.match(regexFilter, filename):
-				matches.append(filename)
+				matches.append(filename.split(args.folderName+"/")[0])
 	
+	s = set(matches)
+	matches = list(s)
 	for file in matches:
-		folder = file.split(args.folderName+"/")[0]
 		originalFolder = folder + args.folderName
 		renamedFolder = folder + args.replacementName
 		print originalFolder
-		os.rename(originalFolder, renamedFolder)
 		print renamedFolder
 
 if __name__ == "__main__":
