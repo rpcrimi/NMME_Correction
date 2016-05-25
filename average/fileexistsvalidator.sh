58d82c5211c2da86be4539f2345d4c686e93e1ae
Robert Crimi <rpcrimi@gmail.com>
2016-05-25 14:44:57 -0600

    cron job

---
+++ average/fileexistsvalidator.sh
@@ -0,0 +1,8 @@
+#!/bin/ksh 
+set -xe
+
+read YYYY MM DD <<<$(date +'%Y %m %d')
+echo $DD
+echo $MM
+
+#python fileexistsvalidator.py -s ../../output1/NCAR/ -m CESM1 -f day -d [[[1980,1],[2010,12]]] -r atmos -l logfiles_cesm1_may9_day -u
