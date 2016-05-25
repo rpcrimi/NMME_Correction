031f16eea433d7682529355b4015af0a995b819e
Robert Crimi <rpcrimi@gmail.com>
2016-05-25 14:52:15 -0600

    moved shell script

--- fileexistsvalidator.sh
+++ fileexistsvalidator.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh 
 
-DD=$(date +%d)_$(date +%m)
+DD=$(date +%Y)_$(date +%d)_$(date +%m)
 echo $DD
 
 #python fileexistsvalidator.py -s ../../output1/NCAR/ -m CESM1 -f day -d [[[1980,1],[2010,12]]] -r atmos -l logfiles_cesm1_may9_day -u
