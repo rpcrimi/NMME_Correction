06f0ae306d2dff9e7ffc72ebfe382bdeb03f0f9c
Robert Crimi <rpcrimi@gmail.com>
2016-06-14 11:19:07 -0600

    updated fileexistsvalidator

--- fileexistsvalidator.sh
+++ fileexistsvalidator.sh
@@ -2,6 +2,7 @@
 
 DD=$(date +%Y)_$(date +%m)_$(date +%d)
 fileexistsvalidator=/datazone/nmme/convert_final/NMME_Correction/fileexistsvalidator.py
+workingdir=/datazone/nmme/convert_final/NMME_Correction
 
 institutions=(NCAR NASA-GMAO NOAA-GFDL CCCMA CCCMA NCEP)
 models=(CESM1 GEOS-5 CCSM4 FLORB-01 CanCM3 CanCM4 CFSV2-2011)
@@ -15,8 +16,8 @@ do
 	mkdir /datazone/nmme/convert_final/NMME_Correction/cron_logfiles/${models[$i]}/logs_$DD/day/
 	mkdir /datazone/nmme/convert_final/NMME_Correction/cron_logfiles/${models[$i]}/logs_$DD/mon/
 
-	python $fileexistsvalidator -s /datazone/nmme/output1/${institutions[$i]}/ -m ${models[$i]} -f 3hr -r atmos -d ${years[$i]} -l /datazone/nmme/convert_final/NMME_Correction/cron_logfiles/${models[$i]}/logs_$DD/3hr/ -u
-	python $fileexistsvalidator -s /datazone/nmme/output1/${institutions[$i]}/ -m ${models[$i]} -f 6hr -r atmos -d ${years[$i]} -l /datazone/nmme/convert_final/NMME_Correction/cron_logfiles/${models[$i]}/logs_$DD/6hr/ -u
-	python $fileexistsvalidator -s /datazone/nmme/output1/${institutions[$i]}/ -m ${models[$i]} -f day -r atmos -d ${years[$i]} -l /datazone/nmme/convert_final/NMME_Correction/cron_logfiles/${models[$i]}/logs_$DD/day/ -u
-	python $fileexistsvalidator -s /datazone/nmme/output1/${institutions[$i]}/ -m ${models[$i]} -f mon -r ocean -d ${years[$i]} -l /datazone/nmme/convert_final/NMME_Correction/cron_logfiles/${models[$i]}/logs_$DD/mon/ -u
+	python $fileexistsvalidator -s /datazone/nmme/output1/${institutions[$i]}/ -m ${models[$i]} -f 3hr -r atmos -d ${years[$i]} -l $workingdir/cron_logfiles/${models[$i]}/logs_$DD/3hr/ -u
+	python $fileexistsvalidator -s /datazone/nmme/output1/${institutions[$i]}/ -m ${models[$i]} -f 6hr -r atmos -d ${years[$i]} -l $workingdir/cron_logfiles/${models[$i]}/logs_$DD/6hr/ -u
+	python $fileexistsvalidator -s /datazone/nmme/output1/${institutions[$i]}/ -m ${models[$i]} -f day -r atmos -d ${years[$i]} -l $workingdir/cron_logfiles/${models[$i]}/logs_$DD/day/ -u
+	python $fileexistsvalidator -s /datazone/nmme/output1/${institutions[$i]}/ -m ${models[$i]} -f mon -r ocean -d ${years[$i]} -l $workingdir/cron_logfiles/${models[$i]}/logs_$DD/mon/ -u
 done
