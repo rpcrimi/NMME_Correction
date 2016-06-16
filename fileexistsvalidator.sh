5554c15f1b77685a9781505df3b238ef45f0abf9
Robert Crimi <rpcrimi@gmail.com>
2016-06-16 09:59:00 -0600

    fixed fileexists bug

--- fileexistsvalidator.sh
+++ fileexistsvalidator.sh
@@ -6,7 +6,7 @@ workingdir=/datazone/nmme/convert_final/NMME_Correction
 
 institutions=(UM-RSMAS NCAR NASA-GMAO NOAA-GFDL CCCMA CCCMA NCEP)
 models=(CCSM4 CESM1 GEOS-5 FLORB-01 CanCM3 CanCM4 CFSV2-2011)
-years=([[[1980,1],[2010,12]]] [[[1982,1],[2012,12]]] [[[1982,1],[2015,10]]] [[[1980,1],[2014,7]]] [[[1981,1],[2012,8]]] [[[1981,1],[2012,8]]] [[[1982,1],[2010,12]]])
+years=([[[1982,1],[2015,10]]] [[[1980,1],[2010,12]]] [[[1982,1],[2012,12]]] [[[1980,1],[2014,7]]] [[[1981,1],[2012,8]]] [[[1981,1],[2012,8]]] [[[1982,1],[2010,12]]])
 
 for i in {0..5} 
 do
