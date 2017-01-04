deaefff80cb68f997baad3ce72b7a46d2f389453
Robert Crimi <rpcrimi@gmail.com>
2017-01-04 11:20:23 -0700

    identify missing 50 hPa variables

--- identifymissing50hpa.py
+++ identifymissing50hpa.py
@@ -1,9 +1,10 @@
 import os
 import re
 
-srcDir = "/datazone/nmme/output1/UM-RSMAS/CCSM4/"
+srcDir = "/datazone/nmme/output1/UM-RSMAS/CCSM4/19820101/"
 regexFilter = re.compile(".*(g|ta|ua|va)_day.*")
 
+print "Gathering Files..."
 matches = []
 for root, dirnames, files in os.walk(srcDir):
 	# Find all filenames with .nc type
@@ -12,4 +13,10 @@ for root, dirnames, files in os.walk(srcDir):
 		if filename.endswith(('.nc', '.nc4')) and re.match(regexFilter, filename):
 				matches.append(filename)
 
-print matches
+print "Analyzing Files..."
+i = 1
+widgets = ['Percent Done: ', Percentage(), ' ', AnimatedMarker(), ' ', ETA()]
+bar = ProgressBar(widgets=widgets, maxval=len(matches)).start()
+for f in matches
+	bar.update(i)
+	i = i + 1
